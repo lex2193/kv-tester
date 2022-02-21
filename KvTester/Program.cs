ab7117e3e03fb8afcb7e8badf0a2655d55675f46
lex2193 <lex2193@hotmail.fr>
2022-02-21 17:25:59 +0100

    First commit

---
+++ KvTester/Program.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+var keyVaultName = Environment.GetEnvironmentVariable("KEY_VAULT_NAME");
+Console.WriteLine($"Key Vault Name: {keyVaultName}");
+
+var kvUri = $"https://{keyVaultName}.vault.azure.net";
+Console.WriteLine($"Key Vault URI: {kvUri}");
+
+var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+foreach (var prop in client.GetPropertiesOfSecrets())
+{
+    var secret = client.GetSecret(prop.Name);
+    Console.WriteLine($"{prop.Name}: {secret.Value}");
+}
